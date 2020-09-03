6dafb368bb677ccd90c8bb7199e445a682d76656
Rajneesh Yadav <yadavrajneesh999@gmail.com>
2020-09-03 18:49:26 +0530

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
+      title: 'WTEDU',
       theme: ThemeData(
         primarySwatch: Colors.green,
         accentColor: Colors.amber,
